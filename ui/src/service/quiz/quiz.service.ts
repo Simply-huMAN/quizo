20fc1f726fb56f5ab55a373ac831f3eb506ac470
Aditya <malothaditya006@gmail.com>
2026-02-20 02:16:14 +0530

    UI: Add result endpoint

--- ui/src/service/quiz/quiz.service.ts
+++ ui/src/service/quiz/quiz.service.ts
@@ -7,19 +7,25 @@ import { Submission } from '../../dto/Submission';
   providedIn: 'root'
 })
 export class QuizService {
-  private apiUrl = 'http://localhost:8080/chat';
+  private apiUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
   createQuiz(topic: string) {
     console.log(`Creating quiz for topic: ${topic}`);
 
-    return this.http.post(`${this.apiUrl}/create-form`, { "content": topic });
+    return this.http.post(`${this.apiUrl}/chat/create-form`, { "content": topic });
   }
 
   submitQuiz(submission: Submission) {
     console.log('Submitting quiz answers:', submission);
 
-    return this.http.post(`${this.apiUrl}/submit-form`, submission);
+    return this.http.post(`${this.apiUrl}/form/submit`, submission);
+  }
+
+  getResults(submissionId: string) {
+    console.log(`Fetching results for submission ID: ${submissionId}`);
+
+    return this.http.get(`${this.apiUrl}/form/result?submissionId=${submissionId}`);
   }
 }
